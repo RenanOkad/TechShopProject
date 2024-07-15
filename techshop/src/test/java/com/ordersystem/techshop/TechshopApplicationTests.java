8b3fb5d06e6d419266f5f4d3ee6645efd0792cd0
RenanOkad <renanokada2000@gmail.com>
2024-07-14 21:35:59 -0300

    feat: Esqueleto do Projeto

---
+++ techshop/src/test/java/com/ordersystem/techshop/TechshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordersystem.techshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
